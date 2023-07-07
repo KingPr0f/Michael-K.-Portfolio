69cdd316dc5870236094688ed3dd88c3c46a7623
KingPr0f <strange98752@gmail.com>
2023-07-07 13:50:47 +1000

    findMax2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function checkEven(num) {
-    return num % 2 === 0;
+function findMax(numArray) {
+    let max = numArray[0];
+    for (let i = 1; i < numArray.length; i++) {
+        if (max < numArray[i]) {
+            max = numArray[i];
+        }
+    }
+    return max;
 }
+
+const numbers = [1,2,3,4,5,6,7,8];
+console.log(findMax(numbers));
